013ed8b7294c639bd9e9ae2d3a3803903cd38008
Aleksandr_Sergeev <alexx.sergeev@gmail.com>
2020-11-04 23:11:41 +0500

    stepik continue

--- Exercises/test.py
+++ Exercises/test.py
@@ -1,16 +1,11 @@
-text = []
-classes = {str(i): [0, 0] for i in range(1, 12)}
-
-with open('dataset_3380_5.txt', 'r') as data:
-    for line in data:
-        line = line.strip().split('\t')
-
-        if line[0] in classes:
-            classes[line[0]][0] += int(line[2])
-            classes[line[0]][1] += 1
-
-for key, value in classes.items():
-    if value[0] == 0:
-        print(key, '-')
+def q(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
     else:
-        print(key, value[0] / value[1])
+        return q(n-1, k) + q(n-1, k-1)
+
+
+n, k = map(int, input().split())
+print(q(n, k))

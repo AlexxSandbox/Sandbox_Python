54fd562890d56be2bdee156903c414e30b961a63
Aleksandr_Sergeev <alexx.sergeev@gmail.com>
2020-11-05 22:45:54 +0500

    stepik continued

--- Exercises/test.py
+++ Exercises/test.py
@@ -1,14 +1,17 @@
-class MoneyBox:
+class Buffer:
 
-    def __init__(self, capacity=0):
-        self.capacity = capacity
+    def __init__(self):
+        self.buffer = []
+        self.count = 0
 
-    def can_add(self, v):
-        if self.capacity >= v:
-            return True
-        else:
-            return False
+    def add(self, *args):
+        for arg in args:
+            self.buffer.append(arg)
+            self.count += 1
+            if self.count == 5:
+                print(sum(self.buffer))
+                self.buffer.clear()
+                self.count = 0
 
-    def add(self, v):
-        if self.can_add(v):
-            self.capacity -= v
+    def get_current_part(self):
+        return self.buffer

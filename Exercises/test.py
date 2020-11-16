b6eaa6ac7d7d5284d4ec6c8bf81db010404fc740
Aleksandr_Sergeev <alexx.sergeev@gmail.com>
2020-11-16 23:42:19 +0500

    stepik

--- Exercises/test.py
+++ Exercises/test.py
@@ -1,1 +1,18 @@
-help(list)
+def substr(s, t):
+    k = 0
+    while t in s:
+        k += 1
+        i = s.find(t) + 1
+        s = s[i:]
+    return k
+
+
+def main():
+    s, t = [input() for x in range(2)]
+    print(substr(s, t))
+
+
+assert substr('abababa', 'aba') == 3
+assert substr('abababa', 'abc') == 0
+assert substr('aaaaa', 'a') == 5
+assert substr('abc', 'abc') == 1

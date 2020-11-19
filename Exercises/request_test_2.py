5c9af0f265ca21531c67a9641077dca83cfdab85
Aleksandr_Sergeev <alexx.sergeev@gmail.com>
2020-11-19 22:05:03 +0500

    stepik

--- Exercises/request_test_2.py
+++ Exercises/request_test_2.py
@@ -2,12 +2,14 @@ import re
 import requests
 
 
-pattern = r'<a[^>]*?href=[\"|\'](?:.+://)*([\w|.|-]+\w)'
+pattern = '<a[^>]*?href=[\"|\'](?:\w+://)*([\w|.|-]+\w)'
 
 url = 'http://pastebin.com/raw/7543p0ns'
 res = requests.get(url).text
-with open('urls.txt', 'w') as text:
-    text.write(res)
+
+with open('urls.txt', 'w') as txt:
+    txt.write(res)
+
 result = re.findall(pattern, res)
 result = set(result)
 result = list(result)
